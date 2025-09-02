6c234ea2a04b5327f8c0dda5c73799deadfb291a
Hossam <43291152+Hoss008@users.noreply.github.com>
2025-09-02 20:36:31 +0300

    made the search function fires when the user just start typing no need to hit search button (no focusing on styling now)

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const searchBtn = document.querySelector("#searchBtn");
-const inpt = document.querySelector("#inpt");
-const moviesList = document.querySelector("#moviesList");
+let searchTimeout;
 
-searchBtn.addEventListener("click", function () {
-  if (inpt.value.trim() === "") return;
+inpt.addEventListener("keydown", function () {
+  clearTimeout(searchTimeout);
 
-  fetch(`https://api.imdbapi.dev/search/titles?query=${inpt.value}`)
-    .then((response) => response.json()) // Parse the response as JSON
-    .then((data) => {
-      // console.log(data);
-      const results = data.titles || [];
-      moviesList.innerText = " ";
-      results.forEach((element) => {
-        const newLi = document.createElement("li");
-        newLi.innerText = element.primaryTitle;
-        const newPic = document.createElement("img");
-        newPic.src = element.primaryImage.url;
-        newPic.classList.add("movie-poster");
-        newLi.append(newPic);
-        moviesList.append(newLi);
-      });
-    }) // Process the data
-    .catch((error) => console.error("Error:", error));
+  searchTimeout = setTimeout(() => {
+    if (inpt.value.trim() === "") return;
+
+    fetch(`https://api.imdbapi.dev/search/titles?query=${inpt.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const results = data.titles || [];
+        moviesList.innerText = " ";
+        results.forEach((element) => {
+          const newLi = document.createElement("li");
+          newLi.innerText = element.primaryTitle;
+          const newPic = document.createElement("img");
+          newPic.src = element.primaryImage.url;
+          newPic.classList.add("movie-poster");
+          newLi.append(newPic);
+          moviesList.append(newLi);
+        });
+      })
+      .catch((error) => console.error("Error:", error));
+  }, 500);
 });
